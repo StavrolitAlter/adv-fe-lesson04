2821d13dca4772f7f0f3b90d8b95b2ff9a00bbbc
Oleksandr Sarzhynskyi <stavorlit@gmail.com>
2016-08-25 22:03:24 +0300

    lesson04

---
+++ client_src/containers/gift-tunner/gift-tunner.js
@@ -0,0 +1,36 @@
+var Bar = require('components/bar/bar.js');
+var TuneControls = require('components/tune-controls/tune-controls.js');
+
+module.exports = function GiftTunner(options) {
+
+	var elem = $('<div class="gift-tunner"></div>');
+	var resourceModel = options.resourceModel;
+	var tunnerName = resourceModel.get('name');
+
+	var bar = new Bar({
+		model: resourceModel
+	});
+	var tunner = new TuneControls({
+		model: resourceModel
+	});
+
+	function render() {
+		elem.html(App.templates['gift-tunner']({
+			name: tunnerName
+		}));
+
+		elem.find('.gift-tunner__bar').html(bar.render().elem);
+		elem.find('.gift-tunner__controls').html(tunner.render().elem);
+
+		return this;
+	}
+
+	return {
+		render: render,
+		elem: elem,
+		name: tunnerName,
+		getCount: function() {
+			return resourceModel.getCount();
+		}
+	};
+};
